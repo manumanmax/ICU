99b08432453a5d8e96bd6ed72a62d63b803f5af9
manumanmax <manua380@hotmail.fr>
2016-04-05 23:23:53 +0200

    Test on camera

--- src/CamTest.py
+++ src/CamTest.py
@@ -5,3 +5,14 @@ Created on 5 avr. 2016
 '''
 
 
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+if (cap.isOpened()):
+    print "captured" 
+ret, frame = cap.read()
+cv2.imshow('first capture',frame)
+#else
+cv2.waitKey(0)
+cv2.destroyAllWindows()
