26b40ccfed91e17009684255c5f386601e16c251
manumanmax <manua380@hotmail.fr>
2016-04-06 20:10:22 +0200

    moving resources and adding treatment functions

---
+++ src/TreatmentFunctions.py
@@ -0,0 +1,22 @@
+'''
+Created on 6 avr. 2016
+
+@author: Emmanuel
+'''
+import cv2
+
+print "\n importing treatment functions :"
+print "  - Face Detection"
+def faceDetection(image):
+    face_cascade = cv2.CascadeClassifier('../resources/classifier/haarcascade_frontalface_alt.xml')
+    eye_cascade = cv2.CascadeClassifier('../resources/classifier/haarcascade_eye.xml')
+    
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = image[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
