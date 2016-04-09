0379223b080c6936327b03e6b3bedd5909491d75
manumanmax <manua380@hotmail.fr>
2016-04-09 20:04:10 +0000

    changing for stream

--- src/CamTest.py
+++ src/CamTest.py
@@ -4,11 +4,13 @@ Created on 5 avr. 2016
 @author: Emmanuel
 '''
 
-    
+import numpy as np
 import socket
 import time
+
 import cv2
 import TreatmentFunctions as tf
+import io
 
 if socket.gethostname() == "comput1":
     print "je tourne sur le PC"
@@ -20,14 +22,25 @@ if socket.gethostname() == "comput1":
     cv2.waitKey(0)
 else:
     from picamera.array import PiRGBArray
-    from picamera import PiCamera
-    
-    camera = PiCamera()
-    camera.resolution = (640, 480)
-    camera.framerate = 15
-    rawCapture = PiRGBArray(camera, size=(640,480))
+    import picamera
 
-    time.sleep(0.1)
+
+
+    stream = io.BytesIO()
+    with picamera.PiCamera() as camera:
+    
+        camera.resolution = (640, 480)
+        camera.framerate = 24
+        camera.start_preview()
+        time.sleep(2)
+        camera.capture(stream, format='jpeg')
+
+    data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+    image = cv2.imdecode(data,1)
+    image = image[:,:,::-1]
+    cv.imshow('Capture', image)
+'''
+    
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
         image = frame.array
         tf.faceDetection(image)
@@ -36,3 +49,4 @@ else:
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
             break
+'''
--- src/TreatmentFunctions.py
+++ src/TreatmentFunctions.py
@@ -4,12 +4,14 @@ Created on 6 avr. 2016
 @author: Emmanuel
 '''
 import cv2
+import time
 
 print "\n importing treatment functions :"
 print "  - Face Detection"
 def faceDetection(image):
+    start = time.time()
     face_cascade = cv2.CascadeClassifier('../resources/classifier/haarcascade_frontalface_alt.xml')
-    eye_cascade = cv2.CascadeClassifier('../resources/classifier/haarcascade_eye.xml')
+    #eye_cascade = cv2.CascadeClassifier('../resources/classifier/haarcascade_eye.xml')
     
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -17,6 +19,7 @@ def faceDetection(image):
         cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = image[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        #eyes = eye_cascade.detectMultiScale(roi_gray)
+        #for (ex,ey,ew,eh) in eyes:
+            #cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    print(time.time() - start)
